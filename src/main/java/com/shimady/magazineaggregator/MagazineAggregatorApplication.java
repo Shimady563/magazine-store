31d5326e6d87456b76c296ef2f85720282fdfb11
Maksim Shiryaev <shimady563@gmail.com>
2024-04-28 20:23:20 +0300

    init

---
+++ src/main/java/com/shimady/magazineaggregator/MagazineAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.shimady.magazineaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shimady/magazineaggregator/MagazineAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimady.magazineaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
