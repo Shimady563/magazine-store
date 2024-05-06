82c1bffdf9f13a0af4126bc0959c8c557554ed14
Maksim Shiryaev <shimady563@gmail.com>
2024-05-06 21:11:33 +0300

    fix(articles): allow viewing of magazines and articles to unauthorized users

--- src/main/java/com/shimady/magazineaggregator/config/WebSecurityConfig.java
+++ src/main/java/com/shimady/magazineaggregator/config/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.shimady.magazineaggregator.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.regex.Pattern;
 
 @Configuration
 public class WebSecurityConfig {
@@ -19,7 +23,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/users/sign-up", "/error", "/magazines", "/magazines/{id}", "/articles", "/article/*", "/articles/*").permitAll()
+                        .requestMatchers(new IdPathRequestMatcher()).permitAll()
+                        .requestMatchers("/users/sign-up", "/error", "/magazines", "/articles", "/article/*", "/articles/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -33,5 +38,14 @@ public class WebSecurityConfig {
                         .permitAll()
                 );
         return http.build();
+    }
+
+    private static class IdPathRequestMatcher implements RequestMatcher {
+        private final Pattern pattern = Pattern.compile("^/magazines/\\d+$");
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            return pattern.matcher(request.getRequestURI()).matches();
+        }
     }
 }
